9a12c59cb109b14d09678fe12a95324f4b727ba9
bgfxc4 <bindermarc@gmx.de>
2020-07-14 20:06:34 +0200

    added github list

---
+++ js/generateProjectList.js
@@ -0,0 +1,24 @@
+(function(){
+
+var gitsToDisplay = ['MetaMuffin/marchat', 'MetaMuffin/g-code-transpiler'];
+
+window.onload = function load(){
+    console.log("windwow is loaded");
+    const list = document.getElementById("gitHubList");
+    for(var i = 0; i < gitsToDisplay.length; i++){
+
+
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.href = "https://github.com/" + gitsToDisplay[i];
+        a.className = "gitHubLink";
+        a.target = "_blank";
+
+        var s = gitsToDisplay[i].split('/', 2);
+        a.textContent = s[1] + " by " + s[0];
+        li.appendChild(a);
+        list.appendChild(li);
+    }
+}
+
+})();
