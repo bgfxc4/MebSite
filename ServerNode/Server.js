009a023c9069d8660bfba9f886b4d95d033fc103
bgfxc4 <bindermarc@gmx.de>
2020-07-16 15:33:38 +0200

    testing with server

--- ServerNode/Server.js
+++ ServerNode/Server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-  });
+const users = [];
+
+app.get('/users', (req, res) =>{
+  res.json(users);
+});
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Listening on port 3000!');
 });
