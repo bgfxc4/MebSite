24e52d9a8bed36a0789706a7b0555e817b5bb664
bgfxc4 <bindermarc@gmx.de>
2020-07-17 07:52:52 +0200

    Update websocketClient.js

--- js/websocketClient.js
+++ js/websocketClient.js
@@ -3,7 +3,7 @@
     var username;
     var password;
     //var socket = new WebSocket("ws://marchat.zapto.org:5554");
-    var socket = new WebSocket("ws://localhost:5554");
+    var socket = new WebSocket("ws://marchat.zapto.org:5554");
 
     socket.onopen = function(e) {
         console.log("[open] Connection established");
