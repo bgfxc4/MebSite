1956531ae582b980389de846db5cd76c2d373859
bgfxc4 <bindermarc@gmx.de>
2020-07-28 14:22:13 +0200

    bugfixes with cookies

--- js/marchat/mainLogin.js
+++ js/marchat/mainLogin.js
@@ -76,7 +76,7 @@ function handleMessage(msg){
         if(pckgContent.packet == "login"){
             document.cookie = "username=" + userUsername;
             document.cookie = "password=" + userPassword;
-            document.cookie = "secure";
+            document.cookie = "samesite=" + "secure";
             console.log(document.cookie.toString());
             window.location.href = "chat.html";
         }else if(pckgContent.packet == "register"){
